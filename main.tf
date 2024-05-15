301766c1487207856fae0a2ad489aa94622308c4
Poorna32 <poornachandraga44@gmail.com>
2024-05-15 15:26:03 +0530

    adding policy to the role.

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 module "tools" {
   for_each = var.tools
-  source = "./module"
+  source   = "./module"
 
-  tool_name     = each.key
-  instance_type = each.value["instance_type"]
+  tool_name            = each.key
+  instance_type        = each.value["instance_type"]
+  policy_resource_list = each.value["policy_resource_list"]
 
-  zone_id       = var.zone_id
+  zone_id              = var.zone_id
 
 }
